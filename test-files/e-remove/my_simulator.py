9b163b669dbb2b779ff94ef0eafc19a64f709de3
Itay <itay@email.sc.edu>
2019-11-15 14:22:46 -0500

    regex

--- test-files/e-remove/my_simulator.py
+++ test-files/e-remove/my_simulator.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 with open(sys.argv[1]) as my_file:
     data = my_file.readlines()
@@ -11,15 +12,47 @@ firstLine = list(data[3])
 # print(firstLine)
 
 # firstLine.remove('4')
+firstEs = 0
+firstEe = 0
 
+str = "The rain in Spain { 5 , 4 }"
+x = re.search(r"{((\d*),)*}", str)
 
-for x in range(len(firstLine)):
-    if firstLine[x] == '{':
-        print('')
-    elif firstLine[x] == '}':
-        print('')
-    elif firstLine[x] == ',':
-        print('')
-    else:
-        # print (firstLine[x])
-        del firstLine[x]
+print("The first white-space character is located in position:",x.start())
+# r”{((\d*),)*}”
+
+# for x in range(len(firstLine)):
+#     if firstEe == 0:
+#         if firstLine[x] == '{':
+#             print('')
+#             firstEs+1
+#         elif firstLine[x] == '}':
+#             print('')
+#             firstEe+1
+#         elif firstLine[x] == ',':
+#             print('')
+#         else:
+#             # print (firstLine[x])
+#             firstLine.pop(x)
+#     elif firstEe == 1:
+#         if firstLine[x] == '{':
+#             print('')
+#             firstEs+1
+#         elif firstLine[x] == '}':
+#             print('')
+#             firstEe+1
+#         elif firstLine[x] == ',':
+#             print('')
+#         else:
+#             print (firstLine[x])
+#     else:
+#         if firstLine[x] == '{':
+#             print('')
+#             firstEs+1
+#         elif firstLine[x] == '}':
+#             print('')
+#             firstEe+1
+#         elif firstLine[x] == ',':
+#             print('')
+#         else:
+#             print (firstLine[x])
